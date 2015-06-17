e9f2ff42d60364a613688c6b2e0c87acfe208dbc
Benjamin Porter <bporter@instructure.com>
2015-06-17 08:17:52 -0600

    smarter sql dump file name

--- backup-databases.sh
+++ backup-databases.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env bash
 
-DATE="$(date '+%d-%B-%Y')"
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RESTORE='\033[0m'
+
+DATE_PRI="$(date '+%d-%B-%Y')"
+DATE_ALT="$(date '+%d-%B-%Y-%s')" # useful when taking multiple backups per day
+
+if [ -d "$DATE_PRI" ]; then
+  DATE="$DATE_ALT"
+else
+  DATE="$DATE_PRI"
+fi
+
 mkdir -p "$DATE"
 cd "$DATE"
 
-pg_dump "canvas_development" > "${DATE}-canvas_development.sql"
-pg_dump "canvas_queue_development" > "${DATE}-canvas_queue_development.sql"
+for i in canvas_development canvas_queue_development; do
+  outfile="${DATE}-${i}.sql"
+  pg_dump "$i" > "$outfile"
+  echo -e "${GREEN}* Backed up database $i to $outfile${RESTORE}"
+  echo -e "${BLUE}    - Restore with 'psql $i < $outfile'${RESTORE}"
+done
