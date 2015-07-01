5641acabd29aa11ebe4b29086ec39e4d548f21a0
Benjamin Porter <bporter@instructure.com>
2015-06-30 21:10:09 -0600

    Remove scribd since it isn't used anymore

--- create-database-config.sh
+++ create-database-config.sh
@@ -23,6 +23,6 @@
 
 [ -d config ] || die "Are you in the root directory of your canvas checkout?"
 
-for c in amazon_s3 delayed_jobs domain file_store outgoing_mail security scribd external_migration database; do 
+for c in amazon_s3 delayed_jobs domain file_store outgoing_mail security external_migration database; do
     cp -v "config/$c.yml.example" "config/$c.yml"
 done
