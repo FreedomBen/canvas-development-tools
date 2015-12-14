a8b95190c97d497afec37f95b9e92be3bb108410
Benjamin Porter <bporter@instructure.com>
2015-12-14 13:59:14 -0700

    Add setup-new-docker-repo

---
+++ setup-new-docker-repo.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# This will be put into a more friendly script later
+
+docker-compose build
+docker-compose run --rm web bundle install && \
+docker-compose run --rm web npm install && \
+docker-compose run --rm web bundle exec rake canvas:compile_assets && \
+cp docker-compose/config/* config/ && \
+docker-compose run --rm web bundle exec rake db:create && \
+docker-compose run --rm web bundle exec rake db:initial_setup
