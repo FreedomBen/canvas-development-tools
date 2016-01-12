f9e22723aa885ac406e55c24c82ccd71a10e2ad2
Benjamin Porter <bporter@instructure.com>
2016-01-11 21:43:19 -0700

    Add helpful reminder

--- setup-new-docker-repo.sh
+++ setup-new-docker-repo.sh
@@ -9,3 +9,8 @@ docker-compose run --rm web bundle exec rake canvas:compile_assets && \
 cp docker-compose/config/* config/ && \
 docker-compose run --rm web bundle exec rake db:create && \
 docker-compose run --rm web bundle exec rake db:initial_setup
+
+echo ""
+echo "For the postgres container to work, you need to do:"
+echo "     docker-compose up postgres"
+echo "and let it initialize"
